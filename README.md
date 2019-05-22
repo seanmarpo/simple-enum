394c6aaebcdd88bc1205ad5c491199e4535810df
Sean Marpo <marpo.sean@gmail.com>
2019-05-22 01:47:16 -0700

    Initial project commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # simple-enum
 Making subdomain enumeration great again... or just automating it. Whatever you prefer.
+
+## Requirements
+You will need both amass and aquatone downloaded and in your PATH.
+* [amass](https://github.com/OWASP/Amass)
+    * `go get -u github.com/OWASP/Amass/...`
+    * `snap install amass`
+* [aquatone](https://github.com/michenriksen/aquatone)
+    * `go get -u github.com/michenriksen/aquatone`
+
+## Usage
+1. Perform a subdomain enumeration and screenshot everything found
+    * `python3 simple_enum.py scan -d <domain> | -df <domain_file>`
+2. Perform a subdomain enumeration
+    * `python3 simple_enum.py enumerate -d <domain> | -df <domain_file>`
+3. Screenshot all domains found via enumeration (only works if subdomain enumeration was previously run)
+    * `python3 simple_enum.py capture -d <domain> | -df <domain_file>`
+
+## General Notes
+This is still under development, so there may be some issues. File an issue or better yet, create a PR :)
---
+++ simple_enum.py
@@ -0,0 +1,120 @@
+from pathlib import Path
+import subprocess
+import logging
+import argparse
+
+
+class Enumerate(object):
+    def __init__(self, root_dir='~/simple-enum'):
+        self.root_dir = Path(root_dir).expanduser().resolve()
+
+    def _check_dirs(self):
+        if not self.root_dir.exists():
+            logging.debug(f"{self.root_dir} does not exist. Creating...")
+            self.root_dir.mkdir(parents=True)
+
+        if not self.target_dir.exists():
+            logging.debug(f"{self.target_dir} does not exist. Creating...")
+            self.target_dir.mkdir()
+
+    def _setup(self, domain):
+        self.target_dir = Path(f"{self.root_dir}/{domain}").resolve()
+        self.amass_out = Path(f"{self.target_dir}/amass.out").resolve()
+        self.aquatone_out = Path(f"{self.target_dir}/aquatone").resolve()
+        self._check_dirs()
+
+    def _process_args(self, args):
+        if args.domain_file:
+            with open(args.domain_file, 'r') as file:
+                self.domains = file.read().splitlines()
+        elif args.domain:
+            self.domains = [args.domain]
+
+    def enumerate(self, args):
+        self._process_args(args)
+        for domain in self.domains:
+            logging.info(f"Running subdomain enumeration against:\t{domain}")
+            self._setup(domain)
+            cmd = f"amass -d {domain} -r 1.1.1.1,8.8.8.8 -o {self.amass_out}"
+            try:
+                p = subprocess.run(cmd, check=True, shell=True, capture_output=True)
+            except subprocess.CalledProcessError:
+                logging.error('Amass returned non-zero return code.', exc_info=True)
+                logging.debug(f"Command run: {cmd} - Output: {p.stdout} | {p.stderr}")
+            logging.info(f"Finished enumeration. You can view results at: {self.amass_out}")
+
+    def capture(self, args):
+        self._process_args(args)
+        for domain in self.domains:
+            logging.info(f"Running screenshot capture against:\t{domain}")
+            self._setup(domain)
+            cmd = f"cat {self.amass_out} | aquatone -scan-timeout 500 -ports large -out {self.aquatone_out}"
+            try:
+                p = subprocess.run(cmd, check=True, shell=True, capture_output=True)
+            except subprocess.CalledProcessError:
+                logging.error('Aquatone returned non-zero return code.', exc_info=True)
+                logging.debug(f"Command run: {cmd} - Output: {p.stdout} | {p.stderr}")
+            logging.info(f"Finished capturing. You can view results at: {self.aquatone_out}/aquatone_report.html")
+
+    def scan(self, args):
+        self.enumerate(args)
+        self.capture(args)
+
+
+def setup_logging(log_level):
+    logging.basicConfig(
+        level=log_level,
+        format='%(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('simple-enum.log', mode='w'),
+            logging.StreamHandler()
+        ]
+    )
+
+
+def setup_parsing():
+    parser = argparse.ArgumentParser(
+        prog='simple_enum.py',
+        description='Simply enumerate your targets.'
+    )
+    subparsers = parser.add_subparsers(
+        title='Enumeration task',
+        dest='task'
+    )
+    parser.add_argument(
+        '-d',
+        '--dir',
+        default='~/simple-enum',
+        help='Location of the root directory to store results. Default=~/simple-enum'
+    )
+    parser.add_argument(
+        '--debug',
+        help='Sets logger level to debug',
+        default=logging.INFO,
+        action='store_const',
+        const=logging.DEBUG,
+        dest='log_level'
+    )
+
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    group = parent_parser.add_mutually_exclusive_group()
+    group.add_argument('-d', '--domain', help='Domain to enumerate')
+    group.add_argument('-df', '--domain-file', help='File containing domains to enumerate (newline separated)')
+
+    subparsers.add_parser('scan', parents=[parent_parser])
+    subparsers.add_parser('enumerate', parents=[parent_parser])
+    subparsers.add_parser('capture', parents=[parent_parser])
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = setup_parsing()
+    args = parser.parse_args()
+    setup_logging(args.log_level)
+
+    if args.task:
+        enum = Enumerate(args.dir)
+        getattr(enum, args.task)(args)
+    else:
+        parser.print_help()
